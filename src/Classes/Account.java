d38074af189bb7b7a56cb182580c405fd3a6c4fe
usuario <usuario@pc20-051>
2021-11-12 10:25:24 +0100

    Tercera Versión de la práctica 3. Práctica completa con todos los casos de pruebas hechos y comprobados

--- src/Classes/Account.java
+++ src/Classes/Account.java
@@ -5,6 +5,8 @@
  */
 package Classes;
 
+import Exceptions.IncorrectDataException;
+
 /**
  *
  * @author Álvaro Rofa Aranda
@@ -18,24 +20,47 @@ public class Account {
         this.balance = 500;
     }
     
+    /**
+     * Constructor con dos parámetros
+     * @param initialBalance
+     * @param idAccount 
+     */
     public Account(float initialBalance, int idAccount){
         this.balance = balance;
         this.idAccount = idAccount;
     }
     
+    /**
+     * Devuelve el balance total de mi cuenta
+     * @return 
+     */
     public float balance(){
         return this.balance;
     }
     
     /**
-     * Sacar dinero de la cuenta
+     * Sacar dinero de la cuenta. Si la cantidad que nos quedara fuera negativa, no podremos sacar dinero de la cuenta, además, data debe ser positivo
      * @param data
      * @return 
      */
-    public float withdraw(float data){
+    public float withdraw(float data) throws IncorrectDataException{
+        if(data <= 0){
+            throw new IncorrectDataException("El saldo que se desea retirar es negativo. Introduzca cantidad positiva");
+        }
+        else {
+            //if()
+        }
+        
+        
+        
         return 0;
     }
     
+    /**
+     * Depositamos dinero en la cuenta.
+     * @param data
+     * @return 
+     */
     public float deposit(float data){
         return 0;
     } 
---
+++ src/Exceptions/IncorrectDataException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exceptions;
+
+/**
+ *
+ * @author usuario
+ */
+public class IncorrectDataException extends Exception {
+    public IncorrectDataException(String message){
+        super(message);
+    }
+}
