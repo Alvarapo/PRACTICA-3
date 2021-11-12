1e1105ddfb6e013f6585241204db2d0207cd1147
usuario <usuario@pc20-051>
2021-11-12 09:56:46 +0100

    Primera Versión de la práctica 3. Creación de la class Account (Cuenta) con métodos vacios y atributos finales definidos.

---
+++ src/Classes/Account.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author Álvaro Rofa Aranda
+ */
+public class Account {
+    private float balance;    
+    private final int idAccount;
+    
+    public Account(int idAccount){
+        this.idAccount = idAccount;
+        this.balance = 500;
+    }
+    
+    public Account(float initialBalance, int idAccount){
+        this.balance = balance;
+        this.idAccount = idAccount;
+    }
+    
+    public float withdraw(float data){
+        return 0;
+    }
+    
+    public float deposit(float data){
+        return 0;
+    } 
+    
+}
