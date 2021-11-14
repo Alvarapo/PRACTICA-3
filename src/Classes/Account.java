cf411a168b19ed46a18eebb8020044e898e0fa3d
Álvaro Rofa Aranda <alvaro.rofa@alu.uhu.es>
2021-11-14 19:41:30 +0100

    Commit Final del último documento de prueba

--- src/Classes/Account.java
+++ src/Classes/Account.java
@@ -6,6 +6,7 @@
 package Classes;
 
 import Exceptions.IncorrectDataException;
+import Exceptions.LimitBalanceException;
 
 /**
  *
@@ -26,7 +27,7 @@ public class Account {
      * @param idAccount 
      */
     public Account(float initialBalance, int idAccount){
-        this.balance = balance;
+        this.balance = initialBalance;
         this.idAccount = idAccount;
     }
     
@@ -39,30 +40,45 @@ public class Account {
     }
     
     /**
-     * Sacar dinero de la cuenta. Si la cantidad que nos quedara fuera negativa, no podremos sacar dinero de la cuenta, además, data debe ser positivo
+     * Sacar dinero de la cuenta.Si la cantidad que nos quedara fuera negativa, no podremos sacar dinero de la cuenta, además, data debe ser positivo
      * @param data
      * @return 
+     * @throws Exceptions.IncorrectDataException Para errores con datos
+     * @throws Exceptions.LimitBalanceException Para errores de limites permitidos
      */
-    public float withdraw(float data) throws IncorrectDataException{
+    public float withdraw(float data) throws IncorrectDataException, LimitBalanceException{
         if(data <= 0){
             throw new IncorrectDataException("El saldo que se desea retirar es negativo. Introduzca cantidad positiva");
+        }else if(data > 500){
+            throw new IncorrectDataException("El saldo que se desea retirar excede de 500€ para " + data + " en withdraw");
         }
+        
         else {
-            //if()
-        }
-        
-        
-        
-        return 0;
+            //SI LA CANTIDAD QUE DESEAMOS SACAR HACE QUE NUESTRA CUENTA QUEDE EN MENOS DE -500.0, SALTARÁ ERROR.
+            if((this.balance-data) < -500.0){
+                throw new LimitBalanceException("El saldo que se desea retirar excede de 500€ para " + data + " en withdraw");
+            }
+            //Si no deja la cuenta en menos de -500.0
+            else{
+                this.balance = this.balance - data;
+            }
+        }        
+        return this.balance;
     }
     
     /**
      * Depositamos dinero en la cuenta.
      * @param data
      * @return 
+     * @throws Exceptions.IncorrectDataException Para errores con datos
      */
-    public float deposit(float data){
-        return 0;
+    public float deposit(float data) throws IncorrectDataException{
+        if(data <= 0){
+            throw new IncorrectDataException("El saldo que se desea ingresar es negativo. Introduzca cantidad positiva");
+        }
+        else this.balance+=data;
+        
+        return this.balance;
     } 
     
 }
--- test/Classes/AccountTest.java
+++ test/Classes/AccountTest.java
@@ -5,6 +5,10 @@
  */
 package Classes;
 
+import Exceptions.IncorrectDataException;
+import Exceptions.LimitBalanceException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import junit.framework.TestCase;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -26,8 +30,8 @@ public class AccountTest extends TestCase{
     
     @BeforeClass
     public void setUpClass() {
-        this.account1 = new Account(12345); //CUENTA 1 DEL EJEMPLO DE LA PRACTICA
-        this.account2 = new Account(67890); //CUENTA 2 DEL EJEMPLO DE LA PRACTICA
+        //this.account1 = new Account(250, 12345); //CUENTA 1 DEL EJEMPLO DE LA PRACTICA
+        //this.account2 = new Account(67890); //CUENTA 2 DEL EJEMPLO DE LA PRACTICA
     }
     
     @AfterClass
@@ -45,31 +49,122 @@ public class AccountTest extends TestCase{
     }
 
     /**
-     * Test of withdraw method, of class Account.
+     * Test of Account1
      */
     @Test
-    public void testWithdraw() {
-        System.out.println("WithDraw-1");
-        float data = 0.0F;
-        float expResult = 0.0F;
-        float result = account1.withdraw(data);
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of deposit method, of class Account.
-     */
-    @Test
-    public void testDeposit() {
-        System.out.println("Deposit-1");
-        float data = 0.0F;
-        float expResult = 0.0F;
-        float result = account1.deposit(data);
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testAccount1() {        
+        System.out.println("Prueba con Account 1------------------------");
+        this.account1 = new Account(50, 12345); //CUENTA 1 DEL EJEMPLO DE LA PRACTICA
+        float result1 = -1;
+        try {
+            System.out.println("1.- WITHDRAW $200 FROM ACCOUNT #12345");
+            account1.withdraw(200);
+            System.out.println("3.- DEPOSIT $100 INTO ACCOUNT #12345");
+            account1.deposit(100);
+            System.out.println("6.- WITHDRAW $200 FROM ACCOUNT #12345");
+            result1 = account1.withdraw(200);
+        } catch (IncorrectDataException | LimitBalanceException ex) {
+            System.out.println(ex.getMessage());
+        }
+        assertEquals(-250f, result1);
+        System.out.println("LOS RESULTADOS EN LA CUENTA 1 HAN SIDO CORRECTOS - BALANCE FINAL DE " + result1);
     }
     
+    /**
+     * Test of Account2
+     */
+    @Test
+    public void testAccount2() {
+        System.out.println("Prueba con Account 2------------------------");
+        this.account2 = new Account(0, 67890); //CUENTA 2 DEL EJEMPLO DE LA PRACTICA
+        float result1 = -1;
+        try {
+            System.out.println("2.- WITHDRAW $350 FROM ACCOUNT #67890");
+            account2.withdraw(350);
+            System.out.println("4.- WITHDRAW $200 FROM ACCOUNT #67890");
+            //account2.withdraw(200);//DA ERROR
+            System.out.println("5.- WITHDRAW $150 FROM ACCOUNT #67890");
+            account2.withdraw(150);
+            System.out.println("7.- DEPOSIT $50 INTO ACCOUNT #67890");
+            result1 = account2.deposit(50);
+            System.out.println("8.- WITHDRAW $100 FROM ACCOUNT #67890");
+            //account2.withdraw(100);//DA ERROR
+        } catch (IncorrectDataException | LimitBalanceException ex) {
+            System.out.println(ex.getMessage());
+        }
+        assertEquals(-450f, result1);
+        System.out.println("LOS RESULTADOS EN LA CUENTA 2 HAN SIDO CORRECTOS - BALANCE FINAL DE " + result1);
+    }
+    
+    /**
+     * Test of Accounts
+     */
+    @Test
+    public void testAccounts() {
+        System.out.println("Ejecucion conjunto de operaciones en ambas cuentas-----------------------");
+        this.account1 = new Account(50, 12345); //CUENTA 1 DEL EJEMPLO DE LA PRACTICA
+        this.account2 = new Account(0, 67890); //CUENTA 2 DEL EJEMPLO DE LA PRACTICA
+        float result1 = -1;
+        float result2 = -1;
+        try {
+            System.out.println("1.- WITHDRAW $200 FROM ACCOUNT #12345");
+            account1.withdraw(200);         
+        } catch (IncorrectDataException | LimitBalanceException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        try {
+            System.out.println("2.- WITHDRAW $350 FROM ACCOUNT #67890");
+            account2.withdraw(350);         
+        } catch (IncorrectDataException | LimitBalanceException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        try {
+            System.out.println("3.- DEPOSIT $100 INTO ACCOUNT #12345");
+            account1.deposit(100);       
+        } catch (IncorrectDataException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        try {
+            System.out.println("4.- WITHDRAW $200 FROM ACCOUNT #67890");
+            account2.withdraw(200);//DA ERROR       
+        } catch (IncorrectDataException | LimitBalanceException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        try {
+            System.out.println("5.- WITHDRAW $150 FROM ACCOUNT #67890");
+            account2.withdraw(150);     
+        } catch (IncorrectDataException | LimitBalanceException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        try {
+            System.out.println("6.- WITHDRAW $200 FROM ACCOUNT #12345");
+            result1 = account1.withdraw(200);    
+        } catch (IncorrectDataException | LimitBalanceException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        try {
+            System.out.println("7.- DEPOSIT $50 INTO ACCOUNT #67890");
+            result2 = account2.deposit(50);  
+        } catch (IncorrectDataException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        try {
+            System.out.println("8.- WITHDRAW $100 FROM ACCOUNT #67890");
+            account2.withdraw(100);//DA ERROR   
+        } catch (IncorrectDataException | LimitBalanceException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        assertEquals(-250f, result1);
+        assertEquals(-450f, result2);
+        System.out.println("LOS RESULTADOS EN LA CUENTA 1 HAN SIDO CORRECTOS - BALANCE FINAL DE " + result1);
+        System.out.println("LOS RESULTADOS EN LA CUENTA 2 HAN SIDO CORRECTOS - BALANCE FINAL DE " + result1);
+    }
 }
